2782e29d401a2580293114e4e7f5e68b1d17882e
Mateusz Ciesielski <ciesielski.mateusz91@gmail.com>
2015-03-28 17:04:36 +0100

    Java containing your objects

---
+++ Nauka_Thinking_in_Java/src/mypackage/MainClass.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplesAndOrangesWithGenerics obj1 = new ApplesAndOrangesWithGenerics();
+	}
+
+}
